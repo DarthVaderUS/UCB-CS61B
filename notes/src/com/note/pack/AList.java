277e5cce7dac5cb8c0fd43988e82d79362a18013
Darth <darth2241@gmail.com>
2025-07-10 09:35:34 +0800

    Interface Inheritance (Last commit)

--- notes/src/com/note/pack/AList.java
+++ notes/src/com/note/pack/AList.java
@@ -18,6 +18,8 @@ public class AList<Item> implements List<Item> {
     }
 
     /* Inserts X into the back of the list */
+    @Override      /* Adds an "@Override" : Reminds Programmer that method definition
+                      is overriding a method in the interface List<Item> */
     public void addLast(Item x){
         if (size == items.length){
             resize(size * 2);
@@ -26,6 +28,7 @@ public class AList<Item> implements List<Item> {
         size++;
     }
 
+    @Override
     public void addFirst(Item x) {
         if (size == items.length) {
             resize(size * 2);
@@ -35,6 +38,7 @@ public class AList<Item> implements List<Item> {
         size++;
     }
 
+    @Override
     public void insert(Item x, int position) {
         if (position < 0 || position > size) {
             throw new IndexOutOfBoundsException();
@@ -47,25 +51,30 @@ public class AList<Item> implements List<Item> {
         size++;
     }
 
+    @Override
     /* Returns the item from the back of the list. */
     public Item getLast(){
         return items[size - 1];
     }
 
     /*Get the ith item in the list, */
+    @Override
     public Item get(int i){
         return items[i];
     }
 
+    @Override
     public int size(){
         return size;
     }
 
+    @Override
     public Item getFirst() {
         if (size == 0) return null;
         return items[0];
     }
 
+    @Override
     public Item removeFirst() {
         if (size == 0) return null;
         Item x = items[0];
@@ -75,6 +84,7 @@ public class AList<Item> implements List<Item> {
         return x;
     }
 
+    @Override
     public Item remove(int position) {
         if (position < 0 || position >= size) {
             throw new IndexOutOfBoundsException();
@@ -87,6 +97,7 @@ public class AList<Item> implements List<Item> {
     }
 
     /* Removes the item from the back of the list and returns it. */
+    @Override
     public Item removeLast(){
         if (size == 0) return null;
         Item x = getLast();
